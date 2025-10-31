314a3d07fc3047528b4d61e59b3bb22ff1c8b27a
Bayram <bmbayrambek1600@gmail.com>
2025-10-31 11:29:23 +0500

    configured settings.py for staticfiles coming up

--- config/settings.py
+++ config/settings.py
@@ -140,12 +140,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 STATICFILES_DIRS: list[str] = []
 
 # Media files
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR / 'media')
 
 # Default primary key field type
